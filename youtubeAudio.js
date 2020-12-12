774536bc030f35b89350fff4a906555a8c559653
AryoP <aryopatel@gmail.com>
2020-12-12 13:22:17 -0600

    tried to make it look a bit cleaner but more work needs to be done on that

--- youtubeAudio.js
+++ youtubeAudio.js
@@ -1,36 +1,51 @@
-let playYT;
+let playYT, startingCount;
+let playing = false;
+startingCount = 0;
 const visualiser = document.getElementById('youtubeVisualiser');
 
 
 const getAudioData = () => {
-    
-    requestAnimationFrame(getAudioData);
+    if(playing){
+        requestAnimationFrame(getAudioData);
 
-    const bufferLength = analyserNode.frequencyBinCount;
-    const dataArray = new Uint8Array(bufferLength);
-
-    analyserNode.getByteFrequencyData(dataArray);
-
+        const bufferLength = analyserNode.frequencyBinCount;
+        const dataArray = new Uint8Array(bufferLength);
     
-    const width = visualiser.width;
-    const height = visualiser.height;
+        analyserNode.getByteFrequencyData(dataArray);
     
-    const canvasContext = visualiser.getContext('2d');
-    //const barWidth =width/bufferLength;
-    const barWidth = width/nonzeroLen(dataArray);
-    canvasContext.clearRect(0,0, width, height);
-
+        
+        const width = visualiser.width;
+        const height = visualiser.height;
+        
+        const canvasContext = visualiser.getContext('2d');
+        const barWidth =width/bufferLength;
+        //const barWidth = width/nonzeroLen(dataArray);
+        canvasContext.clearRect(0,0, width, height);
     
-    dataArray.forEach((item, index) => {
-        if(item > 255){
-            console.log(item);
+        
+        // dataArray.forEach((item, index) => {
+        //     const y = item/255 * height/2;
+        //     const x = barWidth*index;
+    
+        //     canvasContext.fillStyle = `hsl(${y/height * 2 *200}, 100%, 50%)`;
+        //     canvasContext.fillRect(x, height-y, barWidth, y);
+        // })
+        let offset = 0;
+        startingCount += 0.5;
+        for(let i = 0; i < dataArray.length; i++){
+            offset += 1;
+            let y = dataArray[i]/255 * height/2;
+            const x = barWidth*i;
+    
+            // if(y < 40){
+            //     y = 40;
+            // }
+            // y = y + 5*Math.sin(startingCount + offset);
+    
+            canvasContext.fillStyle = `hsl(${i*360/dataArray.length}, 100%, 50%)`;
+            canvasContext.fillRect(x, height-y, barWidth, y);
         }
-        const y = item/255 * height/2;
-        const x = barWidth*index;
-
-        canvasContext.fillStyle = `hsl(${y/height * 2 *200}, 100%, 50%)`;
-        canvasContext.fillRect(x, height-y, barWidth, y);
-    })
+    }
 }
 
 const nonzeroLen = (arr) => {
@@ -43,7 +58,7 @@ const nonzeroLen = (arr) => {
     return count;
 }
 const youtubeAudioCtx  = new AudioContext();
-const audio = new Audio("./testAudio.mp3");
+const audio = new Audio("./words.mp3");
 
 const params = {
     gain: {
@@ -59,7 +74,13 @@ const bassNode = new BiquadFilterNode(youtubeAudioCtx, {
     gain: 0
 })
 
-const analyserNode = new AnalyserNode(youtubeAudioCtx, {fftSize: 256});
+const trebleNode = new BiquadFilterNode(youtubeAudioCtx, {
+    type: 'highshelf',
+    frequency: 1500,
+    gain: 0
+})
+
+const analyserNode = new AnalyserNode(youtubeAudioCtx, {fftSize: 64, smoothingTimeConstant: 0.8});
 
 
 const source = youtubeAudioCtx.createMediaElementSource(audio);
@@ -68,6 +89,7 @@ const source = youtubeAudioCtx.createMediaElementSource(audio);
 
 source
     .connect(bassNode)
+    .connect(trebleNode)
     .connect(analyserNode)
     .connect(youtubeAudioCtx.destination);
 
@@ -75,7 +97,9 @@ source
 
 window.onload = function(){
     playYT = () => {
+        playing = true;
         audio.play();
+        getAudioData();
     }
-    getAudioData();
+
 }
